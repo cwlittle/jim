7e0a84a112ed3d0399885dd44b2d79c741c29717
Charlie Little <cwlittle@utexas.edu>
2021-10-09 15:52:32 -0500

    Add error handling

--- src/lib.rs
+++ src/lib.rs
@@ -5,66 +5,85 @@ use std::io::Write;
 use std::process::{Command, Stdio};
 use toml::Value;
 
-pub fn init(default_path: &str) {
-    let current_exe_path = env::current_exe().unwrap();
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Profile not found: {0}")]
+    ProfileNotFound(String),
+    #[error("Key not found in manifest: {0}")]
+    Manifest(String),
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+    #[error(transparent)]
+    TomlSerialize(#[from] toml::ser::Error),
+    #[error(transparent)]
+    TomlDeserialize(#[from] toml::de::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn init(default_path: &str) -> Result<()> {
+    let current_exe_path = env::current_exe()?;
     let resource_path = current_exe_path.parent().unwrap().join("resources");
     //probably should check if this directory exists first or else it will nuke everthing or panic
-    println!("{:?}", resource_path);
-    fs::create_dir(resource_path.clone()).unwrap();
+    fs::create_dir(resource_path.clone())?;
     let toml_path = resource_path.join("jim.toml");
-    let mut out_file = File::create(toml_path).unwrap();
+    let mut out_file = File::create(toml_path)?;
     let default = format!("[profiles]\ndefault = \"{}\"", default_path);
-    write!(out_file, "{}", default).unwrap();
+    write!(out_file, "{}", default)?;
+    Ok(())
 }
 
-pub fn add_profile(name: &str, path: &str) {
-    let current_exe_path = env::current_exe().unwrap();
+pub fn add_profile(name: &str, path: &str) -> Result<()> {
+    let current_exe_path = env::current_exe()?;
     let resource_path = current_exe_path.parent().unwrap().join("resources");
     let toml_path = resource_path.join("jim.toml");
-    let toml_string = fs::read_to_string(toml_path.clone()).unwrap();
+    let toml_string = fs::read_to_string(toml_path.clone())?;
     let profile_string = format!("\n{} = \"{}\"", name, path);
     let new_toml = toml_string + &profile_string;
-    let mut out_file = File::create(toml_path).unwrap();
-    write!(out_file, "{}", &new_toml).unwrap();
+    let mut out_file = File::create(toml_path)?;
+    write!(out_file, "{}", &new_toml)?;
+    Ok(())
 }
 
-pub fn list_profiles() {
-    let profiles = read_profiles();
+pub fn list_profiles() -> Result<()> {
+    let profiles = read_profiles()?;
     for item in profiles.as_table().iter() {
         for (k, _) in item.iter() {
             println!("\t{}", k);
         }
     }
+    Ok(())
 }
 
-pub fn run_profile(profile: &str) {
-    let config = read_profiles();
+pub fn run_profile(profile: &str) -> Result<()> {
+    let config = read_profiles()?;
     let default_path: String = match config.get(profile) {
-        Some(path) => toml::to_string(path).unwrap(),
-        None => panic!("Profile does not exist"),
+        Some(path) => toml::to_string(path)?,
+        None => return Err(Error::ProfileNotFound(profile.to_string())),
     };
-    run_vim(default_path.replace("\"", ""))
+    run_vim(default_path.replace("\"", ""))?;
+    Ok(())
 }
 
-fn read_profiles() -> Value {
-    let current_exe_path = env::current_exe().unwrap();
+fn read_profiles() -> Result<Value> {
+    let current_exe_path = env::current_exe()?;
     let resource_path = current_exe_path.parent().unwrap().join("resources");
     let toml_path = resource_path.join("jim.toml");
     let toml_string = fs::read_to_string(toml_path).unwrap();
-    let config = &toml_string.parse::<Value>().unwrap();
+    let config = &toml_string.parse::<Value>()?;
     match config.get("profiles") {
-        Some(profiles) => profiles.to_owned(),
-        None => panic!("What the fuck have you done to the toml?"),
+        Some(profiles) => Ok(profiles.to_owned()),
+        None => Err(Error::Manifest("profiles".to_string())),
     }
 }
 
-fn run_vim(profile_path: String) {
-    Command::new("nvim")
+fn run_vim(profile_path: String) -> Result<()> {
+    let mut child = Command::new("nvim")
         .arg("-u")
         .arg(profile_path)
         .stdout(Stdio::inherit())
-        .spawn()
-        .expect("Failed to spawn vim instance")
-        .wait()
-        .expect("Error closing vim instance");
+        .spawn()?;
+
+    child.wait()?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::{crate_version, App, Arg, SubCommand};
-// use std::process::exit;
 use jim::*;
+use std::str::from_utf8;
 
 fn main() {
-    let args = App::new("jim")
+    let mut app = App::new("jim")
         .author("cwlittle, <cwlittle@utexas.edu>")
         .about("Named configuration management for vim")
         .version(concat!("version: ", crate_version!()))
@@ -16,21 +16,58 @@ fn main() {
                 .arg(Arg::with_name("path").required(true)),
         )
         .subcommand(SubCommand::with_name("list"))
-        .arg(Arg::with_name("PROFILE").index(1).required(false))
-        .get_matches();
+        .arg(
+            Arg::with_name("PROFILE")
+                .index(1)
+                .conflicts_with_all(&["init", "add", "list", "help"])
+                .required(false),
+        );
+
+    let mut help = Vec::new();
+    app.write_long_help(&mut help).unwrap();
+
+    let args = app.get_matches();
 
     if std::env::args().len() == 1 {
-        run_profile("default");
-    } else if let Some(profile_path) = args.value_of("PROFILE") {
-        run_profile(profile_path);
+        match run_profile("default") {
+            Err(e) => {
+                println!("\tFailed to run default profile\n\tError: {}", e);
+            }
+            _ => {}
+        };
+    } else if let Some(profile) = args.value_of("PROFILE") {
+        match run_profile(profile) {
+            Err(e) => {
+                println!("\tFailed to run profile {}\n\tError: {}", profile, e);
+            }
+            _ => {}
+        };
+        //basically all args that are not subcommands will filter throuhg this
+        //need to return result rather than panic
     } else if let Some(arg) = args.subcommand_matches("add") {
-        add_profile(arg.value_of("name").unwrap(), arg.value_of("path").unwrap());
+        let name = arg.value_of("name").unwrap();
+        let path = arg.value_of("path").unwrap();
+        match add_profile(arg.value_of("name").unwrap(), arg.value_of("path").unwrap()) {
+            Err(e) => {
+                println!("\tFailed to add profile {}: {}\n\tError: {}", name, path, e);
+            }
+            _ => {}
+        };
     } else if let Some(_) = args.subcommand_matches("list") {
-        list_profiles();
+        match list_profiles() {
+            Err(e) => {
+                println!("\tFailed to list profiles\n\tError: {}", e);
+            }
+            _ => {}
+        };
     } else if let Some(arg) = args.subcommand_matches("init") {
-        init(arg.value_of("default_path").unwrap());
+        match init(arg.value_of("default_path").unwrap()) {
+            Err(e) => {
+                println!("\tFailed to initialize jim\n\tError: {}", e);
+            }
+            _ => {}
+        };
     } else {
-        //show help
-        println!("argument invalid. show help");
+        println!("{}", from_utf8(&help).unwrap());
     }
 }
